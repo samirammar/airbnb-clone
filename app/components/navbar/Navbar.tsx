3bff60dad45d328f87be551cbcd40c509ed398aa
Samir Ammar <samirammar.officially@gmail.com>
2023-05-27 01:39:05 +0300

    fix spaces

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 import Container from "../Container"
 import Logo from "./Logo"
+import Search from "./Search"
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
     <div className="w-full fixed bg-white shadow-sm z-10">
         <div className="py-4 border-b-[1px]">
             <Container>
                 <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
                     <Logo />
+                    <Search />
                 </div>
             </Container>
         </div>
