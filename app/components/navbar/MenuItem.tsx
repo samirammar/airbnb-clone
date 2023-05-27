614d013d437bc2aef12cb52379c85fee4871a374
Samir Ammar <samirammar.officially@gmail.com>
2023-05-27 05:48:38 +0300

    create menu item

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+type Props = {
+    label:string,
+    onClick: () => void,
+}
+
+const MenuItem:React.FC<Props> = ({label, onClick}) => {
+  return (
+    <div
+        onClick={onClick}
+        className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+    >{label}</div>
+  )
+}
+
+export default MenuItem
