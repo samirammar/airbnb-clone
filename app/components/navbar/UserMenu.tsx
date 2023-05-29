afe86dd7f3c87e88e4c223c7d2ba435fca8863e1
Samir Ammar <samirammar.officially@gmail.com>
2023-05-29 21:42:06 +0300

    add register menu item

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,44 +1,47 @@
-'use client'
-import {AiOutlineMenu} from 'react-icons/ai'
-import Avatar from '../Avatar'
-import { useCallback, useState } from 'react'
-import MenuItem from './MenuItem'
+"use client";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const registerModal = useRegisterModal();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleOpen = useCallback(() => {
-        setIsOpen(value => !value);
-    },[]);
-    
-    return (
-        <div className="relative">
-            <div className="flex flex-row items-center gap-3">
-                <div
-                    className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full transition cursor-pointer hover:bg-neutral-100"
-                    onClick={()=>{}}
-                >
-                    Airbnb your home
-                </div>
-                <div
-                    className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-                    onClick={toggleOpen}
-                >
-                    <AiOutlineMenu />   
-                    <Avatar />
-                </div>
-            </div>
-            {isOpen && (
-                <div
-                    className='rounded-xl shadow-md absolute right-0 top-12 w-[40vw] md:w-3/4 bg-white overflow-hidden text-sm'
-                >
-                    <div className="flex flex-col cursor-pointer">
-                        <MenuItem label='Login' onClick={()=>{}}/>
-                    </div>
-                </div>
-            )}
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full transition cursor-pointer hover:bg-neutral-100"
+          onClick={() => {}}
+        >
+          Airbnb your home
         </div>
-  )
-}
+        <div
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          onClick={toggleOpen}
+        >
+          <AiOutlineMenu />
+          <Avatar />
+        </div>
+      </div>
+      {isOpen && (
+        <div className="rounded-xl shadow-lg absolute right-0 top-12 w-[40vw] md:w-3/4 bg-white overflow-hidden text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem label="Login" onClick={() => {}} />
+              <MenuItem label="Register" onClick={registerModal.onOpen} />
+            </>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default UserMenu
+export default UserMenu;
