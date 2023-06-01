05219432a29038afb81e267e8cb3dc7f7549c450
Samir Ammar <samirammar.officially@gmail.com>
2023-06-02 01:27:00 +0300

    compelete array of categoeies

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
-import { TbBeach } from "react-icons/tb";
-import { GiWindmill } from "react-icons/gi";
+import { TbBeach, TbMountain } from "react-icons/tb";
+import {
+  GiIsland,
+  GiWindmill,
+  GiBoatFishing,
+  GiForestCamp,
+  GiCastle,
+  GiCaveEntrance,
+  GiCactus,
+  GiBarn,
+} from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
+import { IoDiamond } from "react-icons/io5";
 import { usePathname, useSearchParams } from "next/navigation";
 import Category from "./Category";
 
@@ -21,7 +32,58 @@ const categories = [
     icon: MdOutlineVilla,
     description: "This property is modern!",
   },
+  {
+    label: "Coutryside",
+    icon: TbMountain,
+    description: "This property has the coutryside!",
+  },
+  {
+    label: "Islands",
+    icon: GiIsland,
+    description: "This property is on an islands!",
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is close to a lake!",
+  },
+  {
+    label: "Skiing",
+    icon: GiBoatFishing,
+    description: "This property has skiing activities!",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is in a castle!",
+  },
+  {
+    label: "Comping",
+    icon: GiForestCamp,
+    description: "This property has comping activities!",
+  },
+  {
+    label: "Article",
+    icon: GiCaveEntrance,
+    description: "This property has comping activities!",
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is in the desert!",
+  },
+  {
+    label: "Barns",
+    icon: GiBarn,
+    description: "This property is in the barn!",
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This property is in the luxurious!",
+  },
 ];
+
 const Categories = () => {
   const params = useSearchParams();
   const categoryParam = params?.get("category");
