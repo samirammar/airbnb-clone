f78b10a563df3394afd95c7bb11f95a7997b2207
Samir Ammar <samirammar.officially@gmail.com>
2023-06-02 05:11:17 +0300

    add rent modal to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import { getCurrentUser } from "./actions/getCurrentUser";
 import { User } from "@prisma/client";
+import RentModal from "./components/modals/RentModal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
         <ToasterProvider />
+        <RentModal />
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
