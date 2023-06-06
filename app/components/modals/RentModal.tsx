632d5e697a4b4308cc78c3298b297c28a1aab2a6
Samir Ammar <samirammar.officially@gmail.com>
2023-06-06 07:22:44 +0300

    add description and price section

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,11 +5,15 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 enum STEPS {
   CATEGORY = 0,
@@ -21,8 +25,10 @@ enum STEPS {
 }
 
 const RentModal = () => {
+  const router = useRouter();
   const rentModal = useRentModal();
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -72,6 +78,28 @@ const RentModal = () => {
 
   const onNext = () => {
     setStep((value) => value + 1);
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) return onNext();
+
+    setIsLoading(true);
+
+    axios
+      .post("/api/listings/data")
+      .then(() => {
+        toast.success("Listing Created!");
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose();
+      })
+      .catch(() => {
+        toast.error("something went wrong.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   const actionLabel = useMemo(() => {
@@ -140,14 +168,14 @@ const RentModal = () => {
             title="Rooms"
             subTitle="How many rooms do you have?"
             value={roomCount}
-            onChange={(value) => setCustomValue("geustCount", value)}
+            onChange={(value) => setCustomValue("roomCount", value)}
           />
           <hr />
           <Counter
             title="Bathrooms"
             subTitle="How many bathrooms do you have?"
             value={bathroomCount}
-            onChange={(value) => setCustomValue("geustCount", value)}
+            onChange={(value) => setCustomValue("bathroomCount", value)}
           />
         </div>
       );
@@ -164,7 +192,51 @@ const RentModal = () => {
           />
         </div>
       );
+    } else if (step === STEPS.DESCRIPTION) {
+      return (
+        <div className="flex flex-col gap-8">
+          <Heading
+            title="How would you describe your place?"
+            subTitle="Short and sweet works best!"
+          />
+          <Input
+            id="title"
+            label="Title"
+            register={register}
+            errors={errors}
+            required
+            disabled={isLoading}
+          />
+          <hr />
+          <Input
+            id="description"
+            label="Description"
+            register={register}
+            errors={errors}
+            required
+            disabled={isLoading}
+          />
+        </div>
+      );
     } else {
+      return (
+        <div className="flex flex-col gap-8">
+          <Heading
+            title="Now, set your price"
+            subTitle="How much do you charge per night!"
+          />
+          <Input
+            id="price"
+            label="Price"
+            type="number"
+            formatPrice
+            register={register}
+            errors={errors}
+            required
+            disabled={isLoading}
+          />
+        </div>
+      );
     }
   };
 
@@ -175,7 +247,7 @@ const RentModal = () => {
       actionLabel={actionLabel}
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
       body={bodyContent()}
     />
