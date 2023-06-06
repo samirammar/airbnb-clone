202f1c5ceccdfddad45a3596f0166b0d840eb69b
Samir Ammar <samirammar.officially@gmail.com>
2023-06-06 04:13:41 +0300

    create counter component

---
+++ app/components/inputs/Counter.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useCallback } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+type Props = {
+  title: string;
+  subTitle: string;
+  value: number;
+  onChange: (value: number) => void;
+};
+
+const Counter: React.FC<Props> = ({ title, subTitle, value, onChange }) => {
+  const onAdd = useCallback(() => {
+    onChange(value + 1);
+  }, [onChange, value]);
+
+  const onReduce = useCallback(() => {
+    if (value === 1) return;
+    onChange(value - 1);
+  }, [value, onChange]);
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-col">
+        <div className="font-medium">{title}</div>
+        <div className="font-light text-gray-600">{subTitle}</div>
+      </div>
+      <div className="flex flex-row items-center gap-4">
+        <div onClick={onAdd} className="counter-btn">
+          <AiOutlineMinus />
+        </div>
+        <div className="font-light text-xl text-neutral-600">{value}</div>
+        <div onClick={onReduce} className="counter-btn">
+          <AiOutlinePlus />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
