b37a0fe767c058c4ef67819c95bac71306551964
Samir Ammar <samirammar.officially@gmail.com>
2023-06-07 13:34:08 +0300

    add empty state component and loop in listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import Container from "@/app/components/Container";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
   return (
-    <div>Airbnb</div>
-  )
+    <Container>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 pt-24">
+        {listings.map((listing: any) => {
+          return <div key={listing.id}></div>;
+        })}
+      </div>
+    </Container>
+  );
 }
