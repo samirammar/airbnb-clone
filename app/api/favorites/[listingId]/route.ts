893e9efa10f690e815834167a0269cb69ce60920
Samir Ammar <samirammar.officially@gmail.com>
2023-06-08 13:43:02 +0300

    create delete favorite function

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -31,3 +31,27 @@ export default async function POST(
 
   return NextResponse.json(user);
 }
+
+export async function DELETE(req: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma?.user.update({
+    where: { id: currentUser.id },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
