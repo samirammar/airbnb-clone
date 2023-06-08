5fc32a4fb27aaffefb6e2954c29cdcfd5d5ead34
Samir Ammar <samirammar.officially@gmail.com>
2023-06-08 13:41:47 +0300

    add current user to ListCard component

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,13 @@ export default async function Home() {
     <Container>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 pt-24">
         {listings.map((listing: any) => {
-          return <ListingCard key={listing.id} data={listing} />;
+          return (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          );
         })}
       </div>
     </Container>
