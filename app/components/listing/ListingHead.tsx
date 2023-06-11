6e44f88b380275b542ae57fd24d14601653db538
Samir Ammar <samirammar.officially@gmail.com>
2023-06-11 20:35:15 +0300

    create listing head component

--- app/components/listing/ListingHead.tsx
+++ app/components/listing/ListingHead.tsx
@@ -3,6 +3,8 @@ import useCountries from "@/app/hooks/useCountries";
 import { User } from "@prisma/client";
 import React from "react";
 import Heading from "../Heading";
+import Image from "next/image";
+import HeartButton from "./HeartButton";
 
 type Props = {
   id: string;
@@ -28,6 +30,17 @@ const ListingHead: React.FC<Props> = ({
         title={title}
         subTitle={`${location?.region}, ${location?.label}`}
       />
+      <div className="w-full relative overflow-hidden rounded-xl h-[60vh]">
+        <Image
+          src={imageSrc}
+          fill
+          alt="image"
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
     </div>
   );
 };
