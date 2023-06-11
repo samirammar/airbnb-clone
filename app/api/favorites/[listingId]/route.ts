2bba4db48791fd253c3253424a92d5bcd8cbec0c
Samir Ammar <samirammar.officially@gmail.com>
2023-06-11 20:04:49 +0300

    add params

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -1,14 +1,11 @@
 import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
-
+import prisma from "@/app/libs/prismadb";
 interface IParams {
   listingId?: string;
 }
 
-export default async function POST(
-  req: Request,
-  { params }: { params: IParams }
-) {
+export async function POST(req: Request, { params }: { params: IParams }) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) return NextResponse.error();
