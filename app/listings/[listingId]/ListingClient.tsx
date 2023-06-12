23552da7bfcca65eecb2e68dc6a71da786258bd3
Samir Ammar <samirammar.officially@gmail.com>
2023-06-12 05:12:45 +0300

    add location value component in listing client component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,5 +1,6 @@
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listing/ListingHead";
+import ListingInfo from "@/app/components/listing/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
 import { Listing, Reservation, User } from "@prisma/client";
 import React, { useMemo } from "react";
@@ -25,6 +26,17 @@ const ListingClient: React.FC<Props> = ({ reservation, listing }) => {
             locationValue={listing.locationValue}
             currentUser={listing.user}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              category={category}
+              user={listing.user}
+              description={listing.description}
+              bathroomCount={listing.bathroomCount}
+              roomCount={listing.roomCount}
+              guestCount={listing.geustCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
