b725e23ee8a43862841faee0c7367a98d6c26684
Samir Ammar <samirammar.officially@gmail.com>
2023-06-15 13:35:32 +0300

    pass current user and reservation to listing component

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,6 +3,7 @@ import { getListingById } from "@/app/actions/getListingById";
 import EmptyState from "@/app/components/EmptyState";
 import React from "react";
 import ListingClient from "./ListingClient";
+import { getReservations } from "@/app/actions/getReservations";
 
 type Props = {
   listingId?: string;
@@ -11,10 +12,17 @@ type Props = {
 const ListingPage = async ({ params }: { params: Props }) => {
   const listing = await getListingById(params.listingId);
   const currentUser = await getCurrentUser();
+  const reservations = await getReservations(params);
 
   if (!listing) return <EmptyState />;
 
-  return <ListingClient listing={listing} />;
+  return (
+    <ListingClient
+      listing={listing}
+      currentUser={currentUser}
+      reservations={reservations}
+    />
+  );
 };
 
 export default ListingPage;
