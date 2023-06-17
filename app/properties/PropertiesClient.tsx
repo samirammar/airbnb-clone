a5287e91efc7537252cbcd058c227ad607ae4a8a
Samir Ammar <samirammar.officially@gmail.com>
2023-06-17 12:03:55 +0300

    create properties component

---
+++ app/properties/PropertiesClient.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Listing, Reservation, User } from "@prisma/client";
+import React, { useCallback, useState } from "react";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import ListingCard from "../components/listing/ListingCard";
+
+type Props = {
+  currentUser?: User | null;
+  listings: Listing[];
+};
+
+const PropertiesClient: React.FC<Props> = ({ listings, currentUser }) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/listings/${id}`)
+        .then(() => {
+          toast.success("Listing Deleted!");
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error);
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+  return (
+    <Container>
+      <Heading title="Properties" subTitle="List of your properties" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 mt-10">
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            actionId={listing.id}
+            onAction={onCancel}
+            disabled={deletingId === listing.id}
+            actionLabel="Delete property"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default PropertiesClient;
