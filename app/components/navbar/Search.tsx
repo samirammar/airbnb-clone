9d871f432388260f18d78232e9aeabbdf7fc12f9
Samir Ammar <samirammar.officially@gmail.com>
2023-06-18 11:08:46 +0300

    add search modal hook to search component

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,19 +1,28 @@
-'use client'
-import {BiSearch} from 'react-icons/bi'
+"use client";
+import useSearchModal from "@/app/hooks/useSearchModal";
+import { BiSearch } from "react-icons/bi";
 
 const Search = () => {
-    return (
-        <div className="rounded-full shadow-sm hover:shadow-md border-[1px] cursor-pointer transition w-full md:w-auto py-2">
-            <div className="flex flex-row items-center justify-between">
-                <div className="text-sm font-semibold px-6">Anywhare</div>
-                <div className="hidden md:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">Any week</div>
-                <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-                    <div className="hidden sm:block">Add Guests</div>
-                    <div className="p-2 bg-rose-500 rounded-full text-white"><BiSearch size={18} /></div>
-                </div>
-            </div>
+  const searchModal = useSearchModal();
+  return (
+    <div
+      onClick={searchModal.onOpen}
+      className="rounded-full shadow-sm hover:shadow-md border-[1px] cursor-pointer transition w-full md:w-auto py-2"
+    >
+      <div className="flex flex-row items-center justify-between">
+        <div className="text-sm font-semibold px-6">Anywhare</div>
+        <div className="hidden md:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+          Any week
         </div>
-    )
-}
+        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+          <div className="hidden sm:block">Add Guests</div>
+          <div className="p-2 bg-rose-500 rounded-full text-white">
+            <BiSearch size={18} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
